63b060e79fce6628af39407fbae7b7c236066425
salimv <salimvzqz@gmail.com>
2024-03-27 23:45:30 -0600

    :hammer: Fix verifySession middleware

--- includes/verifySession.php
+++ includes/verifySession.php
@@ -5,9 +5,10 @@ session_start();
 function validateSession() {
     // Verificar si no hay una sesión iniciada
     if (!isset($_SESSION['username'])) {
+        $_SESSION['error'] = "Debes iniciar sesion";
         // Redireccionar al usuario al formulario de inicio de sesión
-        $loginRoute = __DIR__ . '/index.php';
-        header("Location: ".$loginRoute);
+        $loginPath = '../../index.php';
+        header('Location: ' . $loginPath);
         exit();
     }
     // Si hay una sesión iniciada, el usuario está autenticado
